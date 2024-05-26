2a025a68ce17f51018906966b787f8681ca5bf23
marcelloaxlima <marcello_a_lima@yahoo.com>
2024-05-26 16:07:27 -0300

    Filtro para tipo de produto implementado

--- src/main/java/br/com/fiap/soat07/techchallenge01/application/ports/out/persistence/ProdutoRepository.java
+++ src/main/java/br/com/fiap/soat07/techchallenge01/application/ports/out/persistence/ProdutoRepository.java
@@ -6,12 +6,13 @@ import java.util.Set;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
-import br.com.fiap.soat07.techchallenge01.application.domain.enumeration.TipoProdutoEnum;
 import br.com.fiap.soat07.techchallenge01.adapter.out.persistence.mysql.model.ProdutoModel;
+import br.com.fiap.soat07.techchallenge01.application.domain.enumeration.TipoProdutoEnum;
 
-public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
+public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long>, JpaSpecificationExecutor<ProdutoModel> {
 	
 	@Query(
 			  value = "select p1_1.id,p1_1.codigo,p1_1.data_criacao,p1_1.data_exclusao,p1_1.nome,p1_1.tipo_produto,p1_1.ultima_modificacao,p1_1.valor from combo_produtos p1_0 join produtos p1_1 on p1_1.id=p1_0.produtoid where p1_0.comboid=?1", 
@@ -26,9 +27,8 @@ public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
     Optional<ProdutoModel> findById(Long id);
 
     @Query(
-            value = "SELECT * FROM PRODUTOS p WHERE p.tipoProduto = ?1",
-            nativeQuery = true)
-    Page<ProdutoModel> search(TipoProdutoEnum tipo, Pageable pageable);
+            value = "SELECT p FROM ProdutoModel p WHERE p.tipoProduto = ?1")
+    Page<ProdutoModel> searchByTipo(TipoProdutoEnum tipo, Pageable pageable);
 
 
 }
